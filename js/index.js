445a6b2f4305a05e7911cedfc96e6189bf82f18c
WeissDotEXE <52444964+WeissDotEXE@users.noreply.github.com>
2021-01-07 20:46:17 +0200

    Saving to Github

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const wrx = document.querySelector('.wrx');
+const impreza = document.querySelector('.impreza');
+const forester = document.querySelector('.forester');
+const brz = document.querySelector('.brz');
+
+class cardHover{
+    constructor(name,descrip){
+        this.name=name;
+        this.descrip=descrip;
+    }
+    mouseon(){
+        
+    }
+    mouseover(){
+
+    }
+}
+wrx=new cardHover();
+impreza=new cardHover();
+forester=new cardHover();
+brz=new cardHover();
+wrx.name='WRX';
+impreza.name='Impreza';
+forester.name='Forester';
